2cb022dd678177f3e3be633d04210217dcb90a52
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-04-08 01:23:27 +0800

    feat: add option to generate static sites
    In the next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = m()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     // Configure `pageExtensions` to include MDX files
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     // Optionally, add any other Next.js config below
