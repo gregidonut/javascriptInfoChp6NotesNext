43560db7ef0f6d7c4515f5afa9e7d0edb4e18492
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2024-04-09 15:50:14 +0800

    feat: solve task 2 for delayingDecorator
    Also defined the problem description in the mdx

---
+++ src/app/chapterSections/section9/decoratorsAndForwarding/tasks/task2/delayingDecorator/main.ts
@@ -0,0 +1,19 @@
+type delayFuncArg = (arg: string) => void
+
+function delay(f: delayFuncArg, ms: number): delayFuncArg {
+    return function (arg: string): void {
+        setTimeout((): void => {
+            f(arg)
+        }, ms);
+    }
+}
+
+function f(x: string): void {
+    console.log(x)
+}
+
+const f1000 = delay(f, 1000)
+const f1500 = delay(f, 1500)
+
+f1000("test");
+f1500("test2")
